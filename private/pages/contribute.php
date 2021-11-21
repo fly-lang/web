52853dc2bbd2f3c8ef84a8557cbb15975c6723aa
Fly Lang <dev@flylang.org>
2021-11-21 02:20:17 +0100

    update contents

--- private/pages/contribute.php
+++ private/pages/contribute.php
@@ -13,7 +13,7 @@ Contribute Section
         <div class="row">
             <div class="col-lg-12 col-md-12">
                 <div class="content">
-                    Start contributing now, connect to Fly project.
+                    Start contributing now and connect to the Fly project!
                 </div>
                 <div class="content">
                     <h3><a href="https://github.com/fly-lang" class="github"><i class="fa fa-github"></i> GitHub</a></h3>
@@ -26,11 +26,33 @@ Contribute Section
                             and send a <a target="_blank" href="https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/about-branches">Pull Request</a>
                             if you want to contribute as developer.</li>
                         <li>Observe <a href="https://github.com/trein/dev-best-practices/wiki/Git-Commit-Best-Practices" target="_blank">Git rules and best practice</a>
-                            for doing good commits and don't forget the <a href="/code-of-conduct">Code of Conduct</a>.</li>
+                            for doing good commits and don't forget the <a href="/code-of-conduct">Code of Conduct</a>.
+                        </li>
                     </ul>
-                    Use <a href="https://github.com/fly-lang/fly/discussions" target="_blank">Github Discussions</a> to collect information, speak with collaborators, ask and answer,
+                    Use <a href="https://github.com/fly-lang/fly/discussions" target="_blank">Github Discussions</a> to
+                    collect information, speak with collaborators, ask and answer,
                     or simply share your idea.
                     </p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <div class="content">
+                    <h3><a href="https://twitter.com/fly_lang" class="twitter" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></h3>
+                    You can find important announcements, community updates and other news on Fly official Twitter account.<br><br>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <div class="content">
+                    <h3><SCRIPT type='text/javascript'>a = 'dev';
+                            b = 'flylang.org';
+                            c = 'class="email"';
+                            d = '<i class="fa fa-at"></i>';
+                            document.write('<A hre' + 'f="mai' + 'lto:' + a + '@' + b + '" ' + c + '>');
+                            document.write(d + ' Email</a>');
+                        </SCRIPT></h3>
+                    For direct contact with the founder and receive reliable information.<br><br>
                 </div>
             </div>
 
@@ -43,10 +65,8 @@ Contribute Section
 
             <div class="col-lg-6 col-md-6">
                 <div class="content">
-                    <h3><a href="https://twitter.com/fly_lang" class="twitter" target="_blank"><i class="fa fa-twitter"></i> Twitter @fly_lang</a></h3>
-                    This is the Fly project's official account where you can find important announcements, community updates and
-                    other news.<br><br>
-                    
+                    <h3><a href="https://matrix.to/#/#flylang:matrix.org" class="matrix" target="_blank">[matrix]</a></h3>
+                    Enter in chat wit other developers and share your knowledge.
                 </div>
             </div>
         </div>
--- private/pages/project.php
+++ private/pages/project.php
@@ -12,39 +12,37 @@ Project Section
 
         <div class="content">
             <h3>Introduction</h3>
-            <p>Fly born in 2020 as an alternatives to the most important programming languages, with these main requirements:</p>
+            <p>Fly was born in 2020 as an alternative to some of the most popular programming languages, with these main
+                requirements:</p>
             <ul>
-                <li>Speed on running</li>
-                <li>Easy to understand</li>
-                <li>Quick for writing</li>
-                <li>Powerful for all applications</li>
+                <li>To be fast</li>
+                <li>Easy to write and understand</li>
+                <li>Good, memorable syntax that was quick to write</li>
+                <li>To be powerful for all application</li>
             </ul>
-            <p>The guideline was: <br>
-                <em>"This programming language don't has to do anything special but just make things simpler than others."</em><br>
-                If you think your work is light you will feel like you are flying, so was taken all best things from the
-                programming languages and was implemented into Fly.
+            <p><i>Our guideline is that: "This programming language doesn't have to do anything special, but to just
+                    make things simpler than others."</i>
             </p>
         </div>
 
         <div class="content">
             <h3>Top Features</h3>
-            <p>These are some features taken from other languages:</p>
             <ul>
-                <li>Based on LLVM: compiler backend optimized and exportable in multiple platforms.</li>
-                <li>Multi-paradigm: Procedural, Object Oriented and Functional programming</li>
+                <li>Fly is based on <a target="_blank" href="https://llvm.org">LLVM</a>: backend optimized and exportable for multiple platforms.</li>
+                <li>Multi-paradigm: Procedural, Object oriented, functional programming.</li>
                 <li>Duck Typing Inheritance</li>
-                <li>Exception Handling without try-catch statement</li>
-                <li>Native Aggregator Types</li>
-                <li>Package import management based on namespace</li>
-                <li>Open Source</li>
+                <li>Exception handling without try-catch statement</li>
+                <li>Native aggregator types.</li>
+                <li>Package import management based on namespace.</li>
+                <li>Open source</li>
             </ul>
         </div>
 
         <div class="content">
             <h3>Current Status</h3>
-            <p>The project is arrived at the first pre-release 0.0.4 now it is at an <strong>embryonic state</strong>.<br>
-                In the <a target="_blank" href="javascript:void(0);" onclick="window.location.href=config.wiki.url">Official Wiki</a>
-                you can find more details about all working features and state of the works.
+            <p>The project is at an <strong>embryonic state</strong> with Prerelease version.<br>
+                You will be able to find more details about all of the working features and the state of the project in
+                the <a target="_blank" href="<?php echo $WIKI_URL?>>">Official Wiki</a>.
             </p>
         </div>
     </div>

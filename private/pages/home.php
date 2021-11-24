d3c93fb44552123f4494a953b04d22622ac679df
Fly Lang <dev@flylang.org>
2021-11-24 23:41:13 +0100

    Theme update

--- private/pages/home.php
+++ private/pages/home.php
@@ -11,23 +11,25 @@ Intro Section
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <span id="title">Fly</span>
-                <span id="subtitle">Programming Language for writing easily and quickly all types of software.</span>
+                <span class="title">Fly</span>
+                <span class="subtitle">Programming Language for writing easily and quickly all types of software.</span>
             </div>
 
-                <script type="text/javascript">
-                    var packages = <?php echo json_encode($latest) ?>;
-                </script>
+            <script type="text/javascript">
+                var packages = <?php echo json_encode($latest) ?>;
+            </script>
 
-                <div class="container-fluid">
-                    <a id="download-btn" type="button" href="#" onclick="downloadAutoPackage(packages)" class="btn btn-red btn-lg">
-                        <span class="download">Download</span>
-                        <span class="version"><?php echo $latest['version'] ?> <small>(Prerelease)</small></span>
-                    </a>
-                <span id="download-packages">Download packages for
+            <div class="container-fluid">
+                <a type="button" href="#" onclick="downloadAutoPackage(packages)" class="btn download-btn btn-red">
+                    <span class="download">Download</span>
+                    <span class="version"><?php echo $latest['version'] ?> <small>(Prerelease)</small></span>
+                </a>
+                <span class="download-packages">Download packages for
                     <a href="javascript:void(0);" onclick="downloadPackage('<?php echo $latest['windows']['url'] ?>', '<?php echo $latest['windows']['content_type'] ?>')">Windows 64-bit</a>,
                     <a href="javascript:void(0);" onclick="downloadPackage('<?php echo $latest['macos']['url'] ?>', '<?php echo $latest['macos']['content_type'] ?>')">macOS</a>,
-                    <a href="javascript:void(0);" onclick="downloadPackage('<?php echo $latest['linux']['url'] ?>', '<?php echo $latest['linux']['content_type'] ?>')">Linux</a>.</span>
+                    <a href="javascript:void(0);" onclick="downloadPackage('<?php echo $latest['linux']['url'] ?>', '<?php echo $latest['linux']['content_type'] ?>')">Linux</a>.
+                </span>
+            </div>
         </div>
     </div>
 </section>
@@ -36,22 +38,13 @@ Intro Section
 <!--------------------------
 Home Section
 ---------------------------->
-<section>
+<section class="bg-blue">
 
     <div class="container" id="get-started">
-        <h2>Get Started</h2>
+        <h2>- Get Started -</h2>
 
         <div class="content">
-            <h3>Install latest version</h3>
-            <p>
-                <a target="_blank" href="javascript:void(0);" onclick="window.location.href=config.release.url.releases">Download</a>
-                and install the latest version of Fly by following the
-                <a target="_blank" href="javascript:void(0);" onclick="window.location.href=config.wiki.install">installation guide</a>
-            </p>
-        </div>
-
-        <div class="content">
-            <h3>Hello World</h3>
+            <h3>Try Fly</h3>
             <p>To get started with Fly we will use the classic code example of "Hello world" to demonstrate the basic
                 syntax of the Fly programming language.</p>
             <small class="text-muted">main.fly</small>
@@ -67,7 +60,7 @@ Home Section
         </div>
 
         <div class="content">
-            <h3>Learning Fly</h3>
+            <h3>Learn Fly</h3>
             <p>You can start to learn the Fly language from the official
                 <a target="_blank" href="javascript:void(0);" onclick="window.location.href=config.wiki.url">Github Wiki</a>.
                 Here you can find:

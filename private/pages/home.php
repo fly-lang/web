dd269e21660d14c32c8240c27c59dfb1b6230f3a
Fly Lang <dev@flylang.org>
2021-11-27 00:14:18 +0100

    Theme update

--- private/pages/home.php
+++ private/pages/home.php
@@ -44,10 +44,10 @@ Home Section
         <h2>- Get Started -</h2>
 
         <div class="content">
-            <h3>Try Fly</h3>
+            <h3>First Example</h3>
             <p>To get started with Fly we will use the classic code example of "Hello world" to demonstrate the basic
                 syntax of the Fly programming language.</p>
-            <small class="text-muted">main.fly</small>
+            <small class="fly-filename">main.fly</small>
             <pre class="fly-code">
                 <code>
         import std
@@ -62,45 +62,33 @@ Home Section
         <div class="content">
             <h3>Learn Fly</h3>
             <p>You can start to learn the Fly language from the official
-                <a target="_blank" href="javascript:void(0);" onclick="window.location.href=config.wiki.url">Github Wiki</a>.
+                <a target="_blank" href="<? echo $WIKI_URL?>" target="_blank">Github Wiki</a>.
                 Here you can find:
             </p>
             <div class="row">
                 <div class="col-md-4">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <a href="javascript:void(0);" onclick="window.location.href=config.wiki.languageReference">
-                                    Language Reference
-                                </a>
-                            </h5>
-                            <p class="card-text">The main reference to Fly language syntax</p>
-                        </div>
-                    </div>
+                    <h4>
+                        <a href="<? echo $WIKI_LANGUAGE_REFERENCE_URL?>" target="_blank">
+                            Language Reference
+                        </a>
+                    </h4>
+                    <p class="card-text">The main reference to Fly language syntax</p>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <a href="javascript:void(0);" onclick="window.location.href=config.wiki.commandGuide">
-                                    Command Guide
-                                </a>
-                            </h5>
-                            <p class="card-text">The guide to launch Fly command and how to use all different options.</p>
-                        </div>
-                    </div>
+                    <h4>
+                        <a href="<? echo $WIKI_COMMAND_GUIDE_URL?>" target="_blank">
+                            Command Guide
+                        </a>
+                    </h4>
+                    <p class="card-text">The guide to launch Fly command and how to use all different options.</p>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <a href="javascript:void(0);" onclick="window.location.href=config.wiki.programmersManual">
-                                    Programmer's Manual
-                                </a>
-                            </h5>
-                            <p class="card-text">For better understand compiler functions or starting to contribute to project development.</p>
-                        </div>
-                    </div>
+                    <h4>
+                        <a href="<? echo $WIKI_PROGRAMMERS_MANUAL_URL?>" target="_blank">
+                            Programmer's Manual
+                        </a>
+                    </h4>
+                    <p class="card-text">For better understand compiler functions or starting to contribute to project development.</p>
                 </div>
             </div>
         </div>
--- private/template/header.php
+++ private/template/header.php
@@ -1,24 +1,21 @@
-<div id="topbar">
-    <div class="social-links">
-        <a href="https://twitter.com/fly_lang" class="twitter"><i class="fa fa-twitter"></i>
-            <span class="slogan">FOLLOW</span></a>
-        <a href="https://github.com/fly-lang" class="github"><i class="fa fa-github"></i>
-            <span class="slogan">DEVELOP</span></a>
-        <a href="https://matrix.to/#/#flylang:matrix.org" class="matrix-org"><i class="fa fa-matrix-org"></i>
-            <span class="slogan">CHAT</span></a>
-        <a class="email"><i class="fa fa-at"></i>
-            <span class="slogan">EMAIL</span></a>
-    </div>
-</div>
-
 <div class="logo float-left">
     <a href="/" class="scrollto img-fluid"><img src="img/fly_logo_w300.png" alt=""></a>
 </div>
-<nav class="main-nav float-right d-none d-lg-block">
+<div class="container">
+    <nav class="main-nav float-left d-none d-lg-block">
+        <ul>
+            <li <?php echo $page == 'home' ? 'class="active"' : '' ?>><a href="/">Home</a></li>
+            <li <?php echo $page == 'documentation' ? 'class="active"' : '' ?>><a href="/documentation">Documentation</a></li>
+            <li <?php echo $page == 'download' ? 'class="active"' : '' ?>><a href="/download">Download</a></li>
+            <li <?php echo $page == 'community' ? 'class="active"' : '' ?>><a href="/community">Community</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="social-nav float-right">
     <ul>
-        <li <?php echo $page == 'home' ? 'class="active"' : '' ?>><a href="/">Home</a></li>
-        <li <?php echo $page == 'documentation' ? 'class="active"' : '' ?>><a href="/documentation">Documentation</a></li>
-        <li <?php echo $page == 'download' ? 'class="active"' : '' ?>><a href="/download">Download</a></li>
-        <li <?php echo $page == 'community' ? 'class="active"' : '' ?>><a href="/community">Community</a></li>
+        <li><a href="https://twitter.com/fly_lang" class="twitter"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="https://github.com/fly-lang" class="github"><i class="fa fa-github"></i></a></li>
+        <li><a href="https://matrix.to/#/#flylang:matrix.org" class="matrix-org"><i class="fa fa-matrix-org"></i></a></li>
+        <li><a class="email"><i class="fa fa-at"></i></a></li>
     </ul>
-</nav>
+</div>
